b54c69322e40ae30eb62ba158df542e6e3c90663
Gabriel Casselman <gicasselman1@gmail.com>
2020-08-25 09:17:28 -0700

    Created a basic boilerplate, added firebase support

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import * as firebase from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDls0ovLVZ0wVuu2nVj9qdCst8lV-LiGL0",
+    authDomain: "zlink-33007.firebaseapp.com",
+    databaseURL: "https://zlink-33007.firebaseio.com",
+    projectId: "zlink-33007",
+    storageBucket: "zlink-33007.appspot.com",
+    messagingSenderId: "642825966723",
+    appId: "1:642825966723:web:1fad6af361ed10398237b6",
+    measurementId: "G-W09WJ349BY"
+};
+
+firebase.initializeApp(firebaseConfig)
