b96476c6df23e651fd4a05d04976864aba8378c7
Gabriel Casselman <gicasselman1@gmail.com>
2020-09-10 13:46:23 -0700

    added automatic os detection

--- src/components/DeviceSelector.vue
+++ src/components/DeviceSelector.vue
@@ -31,6 +31,21 @@ export default {
       selectedDevice: 0
     };
   },
+  mounted() {
+    if (
+      navigator.userAgent.indexOf("iPhone") != -1 ||
+      navigator.userAgent.indexOf("Android") != -1
+    ) {
+      this.selectDevice(1);
+    } else if (
+      navigator.userAgent.indexOf("Win") != -1 ||
+      navigator.userAgent.indexOf("Mac") != -1
+    ) {
+      this.selectDevice(0);
+    } else {
+      this.selectDevice(2);
+    }
+  },
   methods: {
     selectDevice(device) {
       this.$emit("select-device", device);
